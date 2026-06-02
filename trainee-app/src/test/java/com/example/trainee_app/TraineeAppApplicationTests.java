b0f018a6798ce4c0c127df96b6d353d20d903017
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-02 14:14:30 +0400

    Initial commit

---
+++ trainee-app/src/test/java/com/example/trainee_app/TraineeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trainee_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TraineeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
