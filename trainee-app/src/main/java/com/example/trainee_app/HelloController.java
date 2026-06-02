66b1461fc7b5a1afeff185b7ad4adcf920ad6dc1
Muneera Al Naabi <muneerasaifalnaabi@gmail.com>
2026-06-02 14:26:13 +0400

    Added annotaion GetMapping

--- trainee-app/src/main/java/com/example/trainee_app/HelloController.java
+++ trainee-app/src/main/java/com/example/trainee_app/HelloController.java
@@ -1,10 +1,11 @@
 package com.example.trainee_app;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    @GetMapping("/hello")
     public String welcomeMessage() {
         return "Welcome to Code Creators Spring Boot Training!";
     }
